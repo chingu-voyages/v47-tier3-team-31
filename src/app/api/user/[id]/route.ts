92c3fe6e7ad65f3dd2372cfb01a566a9e05ecf96
LifeAsDev <angelocraft551@gmail.com>
2024-01-23 12:00:17 -0400

    initial commit

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import { connectMongoDB } from '@/lib/mongodb';
+import mongoose from 'mongoose';
+import User from '@/schemas/user';
+
+export async function GET(req: Request, { params }: any) {
+  //to get query use this, here you get query1 for example
+  const { searchParams } = new URL(req.url);
+  const query1 = searchParams.get('query1');
+  await connectMongoDB();
+
+  const id = params.id;
+  const isValidObjectId = mongoose.Types.ObjectId.isValid(id);
+  if (!isValidObjectId) {
+    return NextResponse.json({ message: 'Invalid ObjectId format' }, { status: 400 });
+  }
+
+  const userFound = await User.findById(id).select('-password');
+  if (userFound) return NextResponse.json({ userFound }, { status: 200 });
+  else return NextResponse.json({ message: 'user no exist' }, { status: 404 });
+}
---
+++ src/lib/category.ts
@@ -0,0 +1,11 @@
+enum Category {
+  travelAndOutdoor = 'Travel and Outdoor',
+  socialActivities = 'Social Activities',
+  hobbiesAndPassions = 'Hobbies and Passions',
+  sportAndFitness = 'Sport and Fitness',
+  healthAndWellbeing = 'Health and Wellbeing',
+  technology = 'Technology',
+  artAndCulture = 'Art and Culture',
+  games = 'Games',
+}
+export default Category;
