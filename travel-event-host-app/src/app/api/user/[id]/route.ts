6da55a7dff933abcfe25f88d5a6aad18fd569006
LifeAsDev <angelocraft551@gmail.com>
2024-01-25 20:29:33 -0400

    secureUser type

--- travel-event-host-app/src/app/api/user/[id]/route.ts
+++ travel-event-host-app/src/app/api/user/[id]/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { connectMongoDB } from '@/lib/mongodb';
 import mongoose from 'mongoose';
 import User from '@/schemas/user';
+import { SecureUser } from '@/types/secureUser';
 
 export async function GET(req: Request, { params }: any) {
   //to get query use this, here you get query1 for example
@@ -16,6 +17,7 @@ export async function GET(req: Request, { params }: any) {
   }
 
   const userFound = await User.findById(id).select('-password');
-  if (userFound) return NextResponse.json({ userFound }, { status: 200 });
+  const secureUser: SecureUser = userFound;
+  if (userFound) return NextResponse.json({ secureUser }, { status: 200 });
   else return NextResponse.json({ message: 'user no exist' }, { status: 404 });
 }
