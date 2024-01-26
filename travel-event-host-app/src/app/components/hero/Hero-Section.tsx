4dde98403a81a48ee7a8a91576f236dc723063c3
David Eastmond <davebuck1981@gmail.com>
2024-01-26 18:48:17 -0500

    address comments re: css modules

--- travel-event-host-app/src/app/components/hero/Hero-Section.tsx
+++ travel-event-host-app/src/app/components/hero/Hero-Section.tsx
@@ -1,34 +1,34 @@
 import Image from 'next/image';
-import './style.css';
+import styles from './hero-section.module.css';
 
 export const HeroSection = () => {
   return (
     <div>
-      <div className='crowd-image-container'>
-        <Image src='/images/hero/crowd.svg' alt='crowd' className='crowd-image' fill />
-        <div className='overlay-area'>
-          <div className='overlay-content-container sm:mt-10 lg:mt-44'>
-            <div className='header-container'>
-              <p className='font-bold hero-text-header text-1xl sm:text-3xl lg:text-5xl'>
+      <div className={styles.crowdImageContainer}>
+        <Image src='/images/hero/crowd.svg' alt='crowd' className={styles.crowdImage} fill />
+        <div className={styles.overlayArea}>
+          <div className={styles.overlayContentContainer}>
+            <div className='headerContainer'>
+              <p className='heroTextHeader font-bold text-1xl sm:text-3xl lg:text-5xl'>
                 Explore, Host, and Connect
               </p>
             </div>
-            <div className='body-content-container flex mt-10'>
-              <div className='passport-image-container'>
+            <div className={styles.bodyContentContainer}>
+              <div className={styles.passportImageContainer}>
                 {/* Image of passport goes here */}
                 <Image
                   src='/images/hero/passport.svg'
                   alt='passport'
-                  className='passport-image'
+                  className={styles.passportImage}
                   fill
                 />
               </div>
-              <div className='body-text-container ml-5 mr-3'>
-                <p className='hero-text-body text-sm sm:text-2xl lg:text-4xl'>
+              <div className='bodyTextContainer ml-5 mr-3'>
+                <p className='heroTextBody text-sm sm:text-2xl lg:text-4xl'>
                   Transform your travel experience with our Travel Event Host App.
                 </p>
                 <br></br>
-                <p className='hero-text-body text-sm sm:text-2xl lg:text-4xl'>
+                <p className='heroTextBody text-sm sm:text-2xl lg:text-4xl'>
                   Whether you're a local looking to showcase your city or a traveller seeking unique
                   experiences, we've got you covered.
                 </p>
@@ -36,7 +36,6 @@ export const HeroSection = () => {
             </div>
           </div>
         </div>
-        {/* <div className='cta-container'></div> */}
       </div>
     </div>
   );
