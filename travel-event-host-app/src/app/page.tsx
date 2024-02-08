af5fc21fdca8773ff4098c96224d04e88355c371
MichMatrix <michel.a.s401@gmail.com>
2024-02-07 22:46:35 -0300

    feature: add footer to the end of the page

--- travel-event-host-app/src/app/page.tsx
+++ travel-event-host-app/src/app/page.tsx
@@ -1,6 +1,12 @@
 import Categories from '@/components/categories/categories';
 import Image from 'next/image';
+import Footer from '@/components/footer/Footer';
 
 export default function Home() {
-  return <Categories />;
+  return (
+    <div>
+      <Categories />
+      <Footer />
+    </div>
+  );
 }
