6ea0b3113b1f46154048cb7ccd148a2e508822ef
David Eastmond <davebuck1981@gmail.com>
2024-02-11 22:01:07 -0500

    update user schema and model

--- travel-event-host-app/src/models/user.ts
+++ travel-event-host-app/src/models/user.ts
@@ -1,20 +1,20 @@
 export interface User {
   _id: string;
   id: string;
-  firstName?: string;
+  firstName: string;
+  lastName: string;
   imageUrl?: string;
-  lastName?: string;
   email: string;
   password: string;
-  location: {
-    country: string;
-    state: string;
-    city: string;
-    coords: {
-      lat: number;
-      long: number;
+  location?: {
+    country?: string;
+    state?: string;
+    city?: string;
+    coords?: {
+      lat?: number;
+      long?: number;
     };
   };
   eventIds: string[];
-  admin?: boolean;
+  isAdmin?: boolean;
 }
