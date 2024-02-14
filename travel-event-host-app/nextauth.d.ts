9d5c8c9d8c18e1fda083d76bce50ca3217454608
David Eastmond <davebuck1981@gmail.com>
2024-02-13 20:41:54 -0500

    module extension, context

---
+++ travel-event-host-app/nextauth.d.ts
@@ -0,0 +1,10 @@
+import { User as BakPakUser } from '@/models/user';
+import 'next-auth';
+
+// Overriding the User and Session types from next-auth so we have custom types
+declare module 'next-auth' {
+  interface User extends Partial<BakPakUser> {}
+  interface Session {
+    user?: Partial<User>;
+  }
+}
--- travel-event-host-app/src/lib/context.tsx
+++ travel-event-host-app/src/lib/context.tsx
@@ -1,28 +1,39 @@
 'use client';
+import { Session } from 'next-auth';
 import { useSession } from 'next-auth/react';
 
 import React, { useContext } from 'react';
+import { AuthStatus } from './auth-status';
 
-const OnboardingContext = React.createContext<any>(undefined); // Creating the  context
+interface IAuthenticationContext {
+  status: AuthStatus | string;
+  session: Session | null;
+}
+
+const AuthenticationContext = React.createContext<IAuthenticationContext>({
+  status: AuthStatus.Unauthenticated as string,
+  session: null,
+}); // Creating the  context
 
 export const OnboardingProvider = ({ children }: { children: React.ReactNode }) => {
   const { data: session, status } = useSession();
 
   return (
-    <OnboardingContext.Provider
+    <AuthenticationContext.Provider
       value={{
         status,
         session,
       }}
     >
       {children}
-    </OnboardingContext.Provider>
+    </AuthenticationContext.Provider>
   );
 };
-export const useOnboardingContext = () => {
-  const onboardingContext = useContext(OnboardingContext);
-  if (onboardingContext === undefined) {
+
+export const useAuthContext = () => {
+  const context = useContext(AuthenticationContext);
+  if (context === undefined) {
     throw new Error('useOnboardingContext must be inside a OnboardingProvider');
   }
-  return onboardingContext;
+  return context;
 };
