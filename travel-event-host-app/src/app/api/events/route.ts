abebc9e0f6e37b98b9b8fc5300fc0a4d6b0f3405
David Eastmond <davebuck1981@gmail.com>
2024-02-15 16:06:09 -0500

    events and users api updates

--- travel-event-host-app/src/app/api/events/route.ts
+++ travel-event-host-app/src/app/api/events/route.ts
@@ -28,25 +28,34 @@ export async function POST(req: Request) {
 
   return NextResponse.json({ message: 'event created the id is ' + newEvent.id }, { status: 201 });
 }
-
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
 
-  let page: number = parseInt(searchParams.get('pageNumber') || '1', 10);
-  let pageSize: number = parseInt(searchParams.get('pageSize') || '50', 10);
+  const timeline: string = searchParams.get('timeline') || 'all';
+  const page: number = parseInt(searchParams.get('page') || '1', 10);
+  const pageSize: number = parseInt(searchParams.get('pageSize') || '50', 10);
+
+  let pipeline: any[] = [];
+  if (timeline === 'upcoming') {
+    pipeline = pipeline.concat({
+      $match: { startDate: { $gte: new Date() } },
+    });
+  } else if (timeline === 'past') {
+    pipeline = pipeline.concat({ $match: { startDate: { $lt: new Date() } } });
+  }
+
+  pipeline = pipeline.concat({
+    $facet: {
+      metadata: [{ $count: 'totalCount' }],
+      data: [{ $skip: (page - 1) * pageSize }, { $limit: pageSize }],
+    },
+  });
 
   await connectMongoDB();
-  const allEvents = await EventRepository.aggregate([
-    {
-      $facet: {
-        metadata: [{ $count: 'totalCount' }],
-        data: [{ $skip: (page - 1) * pageSize }, { $limit: pageSize }],
-      },
-    },
-  ]);
+  const results = await EventRepository.aggregate(pipeline);
 
   return NextResponse.json(
-    { totalCount: allEvents[0].metadata[0].totalCount, events: allEvents[0].data },
+    { totalCount: results[0].metadata[0].totalCount, events: results[0].data },
     { status: 200 },
   );
 }
--- travel-event-host-app/src/app/api/users/[id]/events/route.ts
+++ travel-event-host-app/src/app/api/users/[id]/events/route.ts
@@ -5,20 +5,35 @@ import { EventRepository } from '@/schemas/user-event';
 import mongoose from 'mongoose';
 import { NextResponse } from 'next/server';
 
-// This route will return all the events for a user
-// TODO: this route needs to be secured (only the requsting user should be able to see their events)
+// This route will return all the events for a user.
+// Search query can specify 'timeline' ('upcoming'| 'past' | 'all') and 'pageSize' and 'page'
 export async function GET(req: Request, { params }: any) {
   const { id } = params;
+  const { searchParams } = new URL(req.url);
+
+  const page: number = parseInt(searchParams.get('page') || '1', 10);
+  const pageSize: number = parseInt(searchParams.get('pageSize') || '50', 10);
+
+  const timeline: string = searchParams.get('timeline') || 'all';
 
   if (!mongoose.Types.ObjectId.isValid(id))
     return NextResponse.json({ message: 'Invalid ObjectId format' }, { status: 400 });
 
   await connectMongoDB();
-  // Find the user, then get the events associated with the user
   const user = await UserRepository.findById(id);
   if (user) {
-    const { eventIds } = user;
-    const userEvents = await EventRepository.find({ _id: { $in: eventIds } });
+    const baseQuery: any = {
+      'participants.userId': user._id, // Base query defaults to all
+    };
+
+    if (timeline === 'upcoming') {
+      baseQuery['startDate'] = { $gte: new Date() };
+    } else if (timeline === 'past') {
+      baseQuery['startDate'] = { $lt: new Date() };
+    }
+    const userEvents = await EventRepository.find(baseQuery)
+      .limit(pageSize)
+      .skip((page - 1) * pageSize);
     return NextResponse.json(userEvents, { status: 200 });
   }
   return NextResponse.json({ message: 'User not found' }, { status: 404 });
