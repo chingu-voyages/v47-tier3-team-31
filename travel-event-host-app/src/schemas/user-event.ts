fdeef59523e4d382bfad1331539ffb25aaf89762
David Eastmond <davebuck1981@gmail.com>
2024-02-15 10:01:30 -0500

    update schema

--- travel-event-host-app/src/schemas/user-event.ts
+++ travel-event-host-app/src/schemas/user-event.ts
@@ -31,5 +31,5 @@ const userEventSchema = new Schema<UserEvent>(
   { timestamps: true },
 );
 
-export const EventRepository =
+export const EventRepository: mongoose.Model<UserEvent> =
   mongoose.models.UserEvent || mongoose.model<UserEvent>('UserEvent', userEventSchema);
