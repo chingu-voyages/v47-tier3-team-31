149f5e68385624a4ebd8012991e2cd86e6b788cc
David Eastmond <davebuck1981@gmail.com>
2024-02-16 21:12:41 -0500

    add style constants for forms

---
+++ travel-event-host-app/src/app/common-styles/form-field-sizes.ts
@@ -0,0 +1,6 @@
+// These are used in profile update and sign up and sign in forms
+
+export const profileFormHeaderSizes = ['0.7rem', '0.8rem', '0.9rem', '1.2rem', '1.2rem'];
+export const textInputFieldFontSizes = ['11px', '11px', '12px', '14px', '14px'];
+export const textInputFieldHeights = ['2px', '10px', '12px', '14px', '14px'];
+export const textInputPaddings = ['11px', '11px', '12px', '14px', '14px'];
