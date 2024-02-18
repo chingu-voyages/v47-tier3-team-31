213dfa36ca171749a1386326e7a16ea2a2e8b61b
MichMatrix <michel.a.s401@gmail.com>
2024-02-18 10:38:24 -0300

    feature: remove unused import

--- travel-event-host-app/src/app/page.tsx
+++ travel-event-host-app/src/app/page.tsx
@@ -1,6 +1,3 @@
-import SearchSection from '@/components/searchSection/searchSection';
-import Image from 'next/image';
-import Footer from '@/components/footer/Footer';
 import Landing from '@/components/landing/landing';
 
 export default function Home() {
