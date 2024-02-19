62eabb39b6f0367aace7fa17714d8c67e6c89371
David Eastmond <davebuck1981@gmail.com>
2024-02-18 21:21:51 -0500

    profile editor changes

---
+++ travel-event-host-app/src/components/profile-editor/ProfileEditor.tsx
@@ -0,0 +1,342 @@
+import theme from '@/app/theme';
+
+import {
+  profileFormHeaderSizes,
+  textInputFieldFontSizes,
+  textInputFieldHeights,
+} from '@/app/common-styles/form-field-sizes';
+import { FileObjectParams, SpacesFileUploader } from '@/app/integration/spaces-file-uploader';
+import { generateFilename } from '@/app/integration/utils/generate-filename';
+import { profileUpdateValidationSchema } from '@/lib/yup-validators/profile-update/profile-update-validator';
+import { extractValidationErrors } from '@/lib/yup-validators/utils/extract-validation-errors';
+import { SecureUser } from '@/types/secure-user';
+import { Box, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import avatarStyles from '../../app/common-styles/avatar-styles.module.css';
+import { ErrorComponent } from '../ErrorComponent/ErrorComponent';
+import { CustomGenericMuiAvatar } from '../avatar/custom-generic-user-avatar/CustomGenericUserAvatar';
+import UserAvatar from '../avatar/user-avatar/UserAvatar';
+import { CustomTextField, StyledFormFieldSection } from '../custom-fields/CustomFields';
+
+type EditableProfileFields = 'firstName' | 'lastName' | 'bio' | 'imageUrl';
+
+interface ProfileEditorProps {
+  editDisabled?: boolean;
+  user?: Partial<SecureUser>;
+  onProfileUpdate?: (formValues: Record<EditableProfileFields, string>) => void;
+  isLoading?: boolean;
+}
+
+const profilNotEditableContentSizes = ['0.7rem', '0.8rem', '0.9rem', '1.2rem', '1.2rem'];
+
+export function ProfileEditor({ editDisabled, user, onProfileUpdate }: ProfileEditorProps) {
+  const [formValues, setFormValues] = useState<Record<EditableProfileFields, string>>({
+    firstName: user?.firstName || '',
+    lastName: user?.lastName || '',
+    bio: user?.bio || '',
+    imageUrl: user?.imageUrl || '',
+  });
+
+  const [formErrors, setFormErrors] = useState<Record<string, string[]>>({});
+
+  const handleFormFieldBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    // We validate once
+    setFormErrors({});
+    try {
+      profileUpdateValidationSchema.validateSync(formValues, { abortEarly: false });
+    } catch (err: any) {
+      const errors = extractValidationErrors(err);
+      setFormErrors(errors);
+      return;
+    }
+
+    // Validation is successful do a callback here with the formValues
+    onProfileUpdate && onProfileUpdate(formValues);
+  };
+
+  const handleAvatarImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    const randomFileName: string = generateFilename(user?._id!);
+
+    const cdnResolvePath = `${process.env.NEXT_PUBLIC_SPACES_AVATAR_CDN_PATH}/user_avatars/${randomFileName}`;
+
+    if (file) {
+      /* There are a few gotchyas I've discovered when working with the AWS SDK for S3 and Digital Ocean Spaces:
+        1. The 'Bucket' property is the name of the bucket, not the full path to the bucket.
+        2. The 'Key' property is the full path to the file, including the file name. Do not include a leading forward slash.
+        3. Remember that NEXTJS environment variables are not available in the browser by default, so we need to use NEXT_PUBLIC_ to make them available.
+      */
+      const fileParams: FileObjectParams = {
+        Bucket: process.env.NEXT_PUBLIC_SPACES_AVATAR_BUCKET_PATH!,
+        Key: `user_avatars/${randomFileName}`,
+        Body: file,
+        ACL: 'public-read',
+      };
+
+      try {
+        const upLoader: SpacesFileUploader = new SpacesFileUploader();
+        await upLoader.uploadObject(fileParams);
+        console.info('Photo was uploaded', cdnResolvePath);
+        setFormValues({ ...formValues, imageUrl: cdnResolvePath });
+        onProfileUpdate &&
+          onProfileUpdate({
+            firstName: formValues.firstName,
+            lastName: formValues.lastName,
+            bio: formValues.bio,
+            imageUrl: cdnResolvePath, // We update the imageUrl
+          });
+      } catch (e: any) {
+        console.error(e.message);
+      } finally {
+        // Clear the input
+        e.target.value = '';
+      }
+    }
+  };
+
+  return (
+    <Box
+      className='profileEditorMain'
+      sx={{
+        [theme.breakpoints.down(430)]: {
+          width: '430px',
+          marginLeft: '5px',
+          marginRight: '5px',
+        },
+      }}
+    >
+      <Box
+        display='flex'
+        className='contentBodyContainer'
+        width='100%'
+        gap={3}
+        sx={{
+          [theme.breakpoints.down(430)]: {
+            flexDirection: 'column',
+          },
+        }}
+      >
+        <Box className='avatarContainer' alignSelf={'center'}>
+          <UserAvatar
+            user={user}
+            MuiAvatarComponent={<CustomGenericMuiAvatar theme={theme} />}
+            imageClassName={avatarStyles.userAvatar}
+            nameStyles={{
+              color: theme.palette.primary.thirdColorIceLight,
+              fontWeight: 'bold',
+            }}
+          />
+          {!editDisabled && (
+            // Photo upload button
+            <Box display={'flex'} justifyContent={'center'}>
+              <input
+                type='file'
+                accept='image/*'
+                id='icon-button-photo'
+                style={{ display: 'none' }}
+                onChange={handleAvatarImageChange}
+              />
+              <label htmlFor='icon-button-photo'>
+                <Button
+                  variant='outlined'
+                  component='span'
+                  sx={{
+                    color: theme.palette.primary.thirdColorIceLight,
+                    borderColor: theme.palette.primary.thirdColorIceLight,
+                    fontSize: '0.7rem',
+                    textAlign: 'center',
+                    textTransform: 'none',
+                    padding: '5px',
+                    mt: 1,
+                  }}
+                >
+                  Change Avatar
+                </Button>
+              </label>
+            </Box>
+          )}
+        </Box>
+        <Box
+          display='flex'
+          className='profileFormFields'
+          flexDirection={'column'}
+          gap={1}
+          pr={'1%'}
+          sx={{
+            [theme.breakpoints.down(430)]: {
+              marginTop: 2,
+            },
+          }}
+        >
+          <Box
+            display='flex'
+            gap={1}
+            sx={{
+              [theme.breakpoints.down(430)]: {
+                flexDirection: 'column',
+              },
+            }}
+          >
+            <StyledFormFieldSection>
+              {editDisabled ? (
+                <Typography
+                  sx={{
+                    fontSize: profilNotEditableContentSizes,
+                    color: theme.palette.primary.thirdColorIceLight,
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {user?.firstName}
+                </Typography>
+              ) : (
+                <>
+                  <Typography
+                    sx={{
+                      fontSize: profileFormHeaderSizes,
+                    }}
+                    color={theme.palette.primary.thirdColorIceLight}
+                  >
+                    First Name
+                  </Typography>
+
+                  <CustomTextField
+                    placeholder='First Name'
+                    name='firstName'
+                    value={formValues.firstName}
+                    onBlur={handleFormFieldBlur}
+                    onChange={(e) =>
+                      setFormValues({ ...formValues, [e.target.name]: e.target.value })
+                    }
+                    sx={{
+                      width: '100%',
+                      '&&& input': {
+                        maxHeight: textInputFieldHeights,
+                        fontSize: textInputFieldFontSizes,
+                      },
+                    }}
+                  />
+                  <ErrorComponent
+                    fieldName='firstName'
+                    errors={formErrors}
+                    typographyStyles={{
+                      color: 'red',
+                    }}
+                  />
+                </>
+              )}
+            </StyledFormFieldSection>
+            <StyledFormFieldSection>
+              {editDisabled ? (
+                <Typography
+                  sx={{
+                    fontSize: profilNotEditableContentSizes,
+                    color: theme.palette.primary.thirdColorIceLight,
+                    fontWeight: 'bold',
+                  }}
+                >
+                  {user?.lastName}
+                </Typography>
+              ) : (
+                <>
+                  <Typography
+                    sx={{
+                      fontSize: profileFormHeaderSizes,
+                    }}
+                    color={theme.palette.primary.thirdColorIceLight}
+                  >
+                    Last Name
+                  </Typography>
+                  <CustomTextField
+                    placeholder='Last Name'
+                    name='lastName'
+                    onBlur={handleFormFieldBlur}
+                    onChange={(e) =>
+                      setFormValues({ ...formValues, [e.target.name]: e.target.value })
+                    }
+                    value={formValues.lastName}
+                    sx={{
+                      width: '100%',
+                      '&&& input': {
+                        maxHeight: textInputFieldHeights,
+                        fontSize: textInputFieldFontSizes,
+                      },
+                    }}
+                  />
+                  <ErrorComponent
+                    fieldName='lastName'
+                    errors={formErrors}
+                    typographyStyles={{
+                      color: 'red',
+                    }}
+                  />
+                </>
+              )}
+            </StyledFormFieldSection>
+          </Box>
+          <Box>
+            <StyledFormFieldSection
+              sx={{
+                '& .MuiFormControl-root': {
+                  width: '100%',
+                  backgroundColor: theme.palette.primary.thirdColorIceLight,
+                },
+              }}
+            >
+              {editDisabled ? (
+                <Typography
+                  sx={{
+                    fontSize: profilNotEditableContentSizes,
+                    color: theme.palette.primary.charcoal,
+                  }}
+                >
+                  {user?.bio || 'No Bio'}
+                </Typography>
+              ) : (
+                <>
+                  <Typography
+                    sx={{
+                      fontSize: profileFormHeaderSizes,
+                    }}
+                    color={theme.palette.primary.thirdColorIceLight}
+                  >
+                    Bio
+                  </Typography>
+                  <CustomTextField
+                    placeholder='Bio max 255 chars.'
+                    multiline
+                    minRows={6}
+                    maxRows={6}
+                    inputProps={{ maxLength: 255 }}
+                    name='bio'
+                    value={formValues.bio}
+                    onBlur={handleFormFieldBlur}
+                    onChange={(e) =>
+                      setFormValues({ ...formValues, [e.target.name]: e.target.value })
+                    }
+                    sx={{
+                      '& .MuiInputBase-input': {
+                        fontSize: textInputFieldFontSizes,
+                      },
+                      '&.MuiOutlinedInput-root': {
+                        '&&& input': {
+                          width: '100%',
+                        },
+                      },
+                    }}
+                  />
+                  <ErrorComponent
+                    fieldName='bio'
+                    errors={formErrors}
+                    typographyStyles={{
+                      color: 'red',
+                    }}
+                  />
+                </>
+              )}
+            </StyledFormFieldSection>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
