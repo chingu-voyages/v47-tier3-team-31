ca8c35f26d3b101a5f2c8c1461492dc79d0e5d4e
David Eastmond <davebuck1981@gmail.com>
2024-02-21 19:46:44 -0500

    custom calendar picker

---
+++ travel-event-host-app/src/components/calendar-picker/CalendarPicker.tsx
@@ -0,0 +1,36 @@
+import { Box } from '@mui/material';
+import { DateTimePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import dayjs from 'dayjs';
+
+interface CalendarPickerProps {
+  containerStyles?: React.CSSProperties;
+  value: dayjs.Dayjs | null;
+  onDateTimeChange?: (date: dayjs.Dayjs | null) => void;
+}
+export function CalendarPicker({ containerStyles, value, onDateTimeChange }: CalendarPickerProps) {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box sx={{ ...containerStyles }}>
+        <DateTimePicker
+          slotProps={{ textField: { onKeyDown: (e) => e.preventDefault() } }} // This is to stop the user from entering keyboard input
+          value={value}
+          onChange={(date) => onDateTimeChange && onDateTimeChange(date)}
+          sx={{
+            '&&& input': {
+              backgroundColor: 'white',
+            },
+            '& .MuiButtonBase-root': {
+              backgroundColor: 'white',
+              borderRadius: '0px',
+            },
+            '& .MuiInputBase-root': {
+              backgroundColor: 'white',
+            },
+          }}
+        />
+      </Box>
+    </LocalizationProvider>
+  );
+}
