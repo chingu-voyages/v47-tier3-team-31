1c7eb337ff435996dbca81061bb9b075683c6ad1
David Eastmond <davebuck1981@gmail.com>
2024-02-27 20:25:46 -0500

    add new prop for address autocomplete

--- travel-event-host-app/src/components/address-autocomplete/AddressAutocomplete.tsx
+++ travel-event-host-app/src/components/address-autocomplete/AddressAutocomplete.tsx
@@ -5,11 +5,13 @@ import { CustomTextField } from '../custom-fields/CustomFields';
 interface AddressAutocompleteProps {
   onLocationSelected?: (place: google.maps.places.PlaceResult) => void;
   componentName: string;
+  placeholder?: string;
 }
 
 export function AddressAutocomplete({
   onLocationSelected,
   componentName,
+  placeholder,
 }: AddressAutocompleteProps) {
   const { ref: materialRef } = usePlacesWidget({
     apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
@@ -27,7 +29,7 @@ export function AddressAutocomplete({
 
   return (
     <CustomTextField
-      placeholder='Start typing your location'
+      placeholder={placeholder || 'Start typing your location'}
       required
       id={componentName}
       name={componentName}
